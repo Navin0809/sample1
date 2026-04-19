6d85e509ebdde0cf6579ce6ed9f1feb711fbe1b3
naveen <naveensmart0799@gmail.com>
2026-04-19 11:37:23 +0530

    ddf

--- hi.java
+++ hi.java
@@ -3,8 +3,11 @@ public class hi {
     public static void main(String[] args) {
         int a=10;
         int b=5;
+        int c=49;
 
         System.out.println(a+b);
         System.out.println(a-b);
+
+
     }
 }
