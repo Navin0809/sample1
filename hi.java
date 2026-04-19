c2dac898113b17c36ad227d6a5bd4a5c27164dcf
naveensmart0799 <98144901+Navin0809@users.noreply.github.com>
2026-04-19 11:53:20 +0530

    Update hi.java

--- hi.java
+++ hi.java
@@ -8,6 +8,8 @@ public class hi {
         int e=44545;
         int h=56;
         int lop=56;
+        int kiop=90;
+        String aer="Ererre";
 
         System.out.println(a+b);
         System.out.println(a-b);
